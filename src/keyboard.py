e33265168f098c01b6a3f99c0f4cd7351586abbe
Timur_Q <ilyasovtimur2001@gmail.com>
2023-01-19 12:14:07 +0300

    Add auto resize to keyboard entity

--- src/keyboard.py
+++ src/keyboard.py
@@ -5,13 +5,13 @@ from aiogram.types import ReplyKeyboardRemove, \
 button_yes = KeyboardButton('Yes!')
 button_no  = KeyboardButton('No!')
 
-reply_kb = ReplyKeyboardMarkup()
+reply_kb = ReplyKeyboardMarkup(resize_keyboard=True)
 reply_kb.add(button_no)
 reply_kb.add(button_yes)
 
 button_open_cal = KeyboardButton('Open Calendar')
 
-start_kb = ReplyKeyboardMarkup()
+start_kb = ReplyKeyboardMarkup(resize_keyboard=True)
 start_kb.add(button_open_cal)
 
 button1 = KeyboardButton('Statistics')
