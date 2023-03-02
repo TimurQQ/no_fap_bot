e85ea111402c529e9f8234f7268c85ed4eda6627
tilyasov <tilyasov@elvees.com>
2023-03-02 17:37:13 +0300

    Fix BotBlocked exception in daily actions

--- src/handlers/daily_actions.py
+++ src/handlers/daily_actions.py
@@ -2,6 +2,7 @@ from dispatcher import dp, bot
 from aiogram import types
 from aiogram.dispatcher.filters import Text
 from datetime import datetime
+from aiogram.utils import exceptions
 from database import database, blacklist
 from src.keyboard import reply_kb, menu_kb
 import random
@@ -25,28 +26,31 @@ async def checkRating():
         chat = await bot.get_chat(userStat.uid)
         actual_nick = chat.username
         database.update(userStat.uid, newNickName=actual_nick)
-        if days >= 0:
-            if len(userStat.collectedMemes) == 0:
-                new_day = 0
-            else:
-                last_day = int(userStat.collectedMemes[-1].split()[1].split("_")[0])
-                new_day = min(last_day + 1, days)
-                if (last_day == new_day):
+        try:
+            if days >= 0:
+                if len(userStat.collectedMemes) == 0:
+                    new_day = 0
+                else:
+                    last_day = int(userStat.collectedMemes[-1].split()[1].split("_")[0])
+                    new_day = min(last_day + 1, days)
+                    if (last_day == new_day):
+                        continue
+                if (new_day not in database.cached_memes):
+                    if (userStat.isWinner):
+                        continue
+                    database.update(userStat.uid, winnerFlag=True)
+                    await bot.send_message(userStat.uid, f"Not enough memes for you :(", reply_markup=menu_kb)
                     continue
-            if (new_day not in database.cached_memes):
-                if (userStat.isWinner):
-                    continue
-                database.update(userStat.uid, winnerFlag=True)
-                await bot.send_message(userStat.uid, f"Not enough memes for you :(", reply_markup=menu_kb)
-                continue
-            else:
-                database.update(userStat.uid, winnerFlag=False)
-            day_memes = database.cached_memes[new_day]
-            new_meme = random.choice(day_memes)
-            userStat.collectedMemes.append(new_meme)
-            await bot.send_message(userStat.uid, f"Congratulations!!! You have collected {new_day}-day meme.", reply_markup=menu_kb)
-            with open(os.path.join("storage", "memes", new_meme), "rb") as meme_pic:
-                await bot.send_photo(userStat.uid, meme_pic)
+                else:
+                    database.update(userStat.uid, winnerFlag=False)
+                day_memes = database.cached_memes[new_day]
+                new_meme = random.choice(day_memes)
+                userStat.collectedMemes.append(new_meme)
+                await bot.send_message(userStat.uid, f"Congratulations!!! You have collected {new_day}-day meme.", reply_markup=menu_kb)
+                with open(os.path.join("storage", "memes", new_meme), "rb") as meme_pic:
+                    await bot.send_photo(userStat.uid, meme_pic)
+        except exceptions.BotBlocked:
+            print(f'Bot blocked by user @{chat.username}')
     database.update()
 
 async def sendCheckMessageBroadcast():
