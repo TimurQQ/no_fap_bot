d456efcb51e4febb09e335bafa5f69cd1960ba31
Timur_Q <ilyasovtimur2001@gmail.com>
2023-03-02 15:01:18 +0300

    remove creation of __pycache__ files

--- pull_up_changes.sh
+++ pull_up_changes.sh
@@ -3,4 +3,4 @@
 git pull origin master
 kill $(ps aux | grep '[p]ython no_fap.py' | awk '{print $2}')
 cp storage/all_scores_saved.json backup/all_scores_saved.json
-python no_fap.py &
+python -B no_fap.py &
