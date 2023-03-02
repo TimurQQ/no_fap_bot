1b715282d3cf4cd4b9d4f532592850add34b71d5
tilyasov <tilyasov@elvees.com>
2023-03-02 15:25:09 +0300

    fix bash pull up changes script

--- pull_up_changes.sh
+++ pull_up_changes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 git pull origin master
-kill $(ps aux | grep '[p]ython no_fap.py' | awk '{print $2}')
+kill $(ps aux | grep -E '[p]ython\s+-\w+\s+no_fap.py' | awk '{print $2}')
 cp storage/all_scores_saved.json backup/all_scores_saved.json
 python -B no_fap.py &
