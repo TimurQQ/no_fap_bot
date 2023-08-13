33fd5c1fdc80b2bc9175fa4da7ac28f8362de362
Sergey Doroshin <sergeybezzabotnovv@gmail.com>
2023-08-13 20:12:01 +0300

    Refactor mechanism of creation statistics text

--- src/handlers/statistics.py
+++ src/handlers/statistics.py
@@ -7,35 +7,39 @@ from datetime import datetime
 from src.keyboard import getInlineSlider
 from src.keyboard import choosepage_cb
 
+def make_statistics_message(top10List, callerStat):
+    return (
+        "Statistics (1-10):\n"
+        + "\n".join(
+            [
+                f"{i+1}. @{top10List[i].username} Stat: {datetime.now() - top10List[i].lastTimeFap}"
+                for i in range(len(top10List))
+                if top10List[i].username
+            ]
+        )
+        + f"\n...\n{callerStat[0]}. @{callerStat[1].username} Stat: {datetime.now() - callerStat[1].lastTimeFap}"
+    )
+
 @dp.message_handler(Text("Statistics"))
 @dp.message_handler(commands=[commands.StatisticsCommand])
 async def show_stats(message: types.Message):
     top10List, callerStat = database.getTop(caller=message.chat.id)
 
-    await bot.send_message(message.chat.id,
-        "Statistics (1-10):\n" +
-        "\n".join([
-            f"{i+1}. @{top10List[i].username} Stat: {datetime.now() - top10List[i].lastTimeFap}"\
-            for i in range(len(top10List)) if top10List[i].username
-         ]) +
-         f"\n...\n{callerStat[0]}. @{callerStat[1].username} Stat: {datetime.now() - callerStat[1].lastTimeFap}",
-            reply_markup = getInlineSlider(0, message.chat.id))
+    await bot.send_message(
+        message.chat.id,
+        make_statistics_message(top10List, callerStat),
+        reply_markup=getInlineSlider(0, message.chat.id),
+    )
 
 @dp.callback_query_handler(choosepage_cb.filter(direction='next'))
 async def handle_next_page(query: types.CallbackQuery, callback_data: dict):
     next_page = int(callback_data["page"]) + 1
     topListPart, callerStat = database.getTop(page = next_page, caller=callback_data["caller"])
     await bot.edit_message_text(
-        f"Statistics ({next_page*10 + 1}-{(next_page + 1)*10}):\n" +
-        "\n".join([
-                f"{next_page*10 + i + 1}. @{topListPart[i].username} Stat: {datetime.now() - topListPart[i].lastTimeFap}"\
-                for i in range(len(topListPart)) if topListPart[i].username
-            ]
-        ) +
-         f"\n...\n{callerStat[0]}. @{callerStat[1].username} Stat: {datetime.now() - callerStat[1].lastTimeFap}",
+        make_statistics_message(topListPart, callerStat),
         query.from_user.id,
         query.message.message_id,
-        reply_markup=getInlineSlider(next_page, callback_data["caller"])
+        reply_markup=getInlineSlider(next_page, callback_data["caller"]),
     )
 
 @dp.callback_query_handler(choosepage_cb.filter(direction='back'))
@@ -45,13 +49,7 @@ async def handle_prev_page(query: types.CallbackQuery, callback_data: dict):
         return
     topListPart, callerStat = database.getTop(page = prev_page, caller=callback_data["caller"])
     await bot.edit_message_text(
-        f"Statistics ({prev_page*10 + 1}-{(prev_page + 1)*10}):\n" +
-        "\n".join([
-                f"{prev_page*10 + i + 1}. @{topListPart[i].username} Stat: {datetime.now() - topListPart[i].lastTimeFap}"\
-                for i in range(len(topListPart)) if topListPart[i].username
-            ]
-        ) +
-         f"\n...\n{callerStat[0]}. @{callerStat[1].username} Stat: {datetime.now() - callerStat[1].lastTimeFap}",
+        make_statistics_message(topListPart, callerStat),
         query.from_user.id,
         query.message.message_id,
         reply_markup=getInlineSlider(prev_page, callback_data["caller"])
