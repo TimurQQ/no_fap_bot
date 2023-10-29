ce09985a5e93a155928540a178b04299fbc74ed6
Sergey Doroshin <sergeybezzabotnovv@gmail.com>
2023-10-29 16:27:04 +0300

    Add exception error handling messages

--- src/handlers/daily_actions.py
+++ src/handlers/daily_actions.py
@@ -2,7 +2,7 @@ from dispatcher import dp, bot
 from aiogram import types
 from aiogram.dispatcher.filters import Text
 from datetime import datetime
-from aiogram.utils import exceptions
+from aiogram.utils.exceptions import BotBlocked, ChatNotFound, RetryAfter, TelegramAPIError
 from database import database
 from src.keyboard import reply_kb, menu_kb
 import random
@@ -36,82 +36,80 @@ async def fapping_reply(message: types.Message):
     await message.reply(message_text, reply_markup=menu_kb)
 
 
-def memeExistsForUser(userStat, new_day) -> bool:
+def memeExistsForUser(user, new_day) -> bool:
     if (new_day in database.cached_memes):
         return True
-    if (not userStat.isWinner):
-        database.update(userStat.uid, winnerFlag=True)
+    if (not user.isWinner):
+        database.update(user.uid, winnerFlag=True)
     return False
 
 
-async def sendMemeToUser(userStat, new_day):
+async def sendMemeToUser(user, new_day):
     day_memes = database.cached_memes[new_day]
     new_meme = random.choice(day_memes)
-    userStat.collectedMemes.append(new_meme)
-    noFapLogger.info(f"User {userStat.username}({userStat.uid}) gets meme {new_meme}")
+    user.collectedMemes.append(new_meme)
+    noFapLogger.info(f"User {user.username}({user.uid}) gets meme {new_meme}")
 
-    await send_message_safety(userStat.uid, f"Congratulations!!! You have collected {new_day}-day meme.", reply_markup=menu_kb)
-    await send_photo_safety(userStat.uid, new_meme)
+    await send_message_safety(user, f"Congratulations!!! You have collected {new_day}-day meme.", reply_markup=menu_kb)
+    await send_photo_safety(user.uid, new_meme)
 
 
-async def sendDailyQuestion(userStat, actual_nick):
-    if userStat.uid in database.getBlackList():
+async def sendDailyQuestion(user, actual_nick):
+    if user.uid in database.getBlackList():
         noFapLogger.info(f'User: "{actual_nick}" has been banned by bot!')
         return
     message = "Did you fap today?"
-    noFapLogger.info(f'Try to send check message: "{message}" to user {actual_nick}({userStat.uid})')
+    noFapLogger.info(f'Try to send check message: "{message}" to user {actual_nick}({user.uid})')
 
-    await send_message_safety(userStat.uid, message, reply_markup=reply_kb,
-        on_success=lambda: database.user_contexts[userStat.uid].daily_check()
+    await send_message_safety(user, message, reply_markup=reply_kb,
+        on_success=lambda: database.user_contexts[user.uid].daily_check()
     )
 
 
 async def checkRating():
-    for userStat in database.data.values():
-        days = (datetime.now() - userStat.lastTimeFap).days
+    for user in database.data.values():
+        days = (datetime.now() - user.lastTimeFap).days
 
-        if (days < 0 or userStat.isBlocked): continue
+        if (days < 0 or user.isBlocked): continue
 
-        uid = userStat.uid
+        uid = user.uid
         chat = await bot.get_chat(uid)
         actual_nick = chat.username
         database.update(uid, newNickName=actual_nick)
 
         new_day = 0
-        if len(userStat.collectedMemes) != 0:
-            last_day = int(userStat.collectedMemes[-1].split()[1].split("_")[0])
+        if len(user.collectedMemes) != 0:
+            last_day = int(user.collectedMemes[-1].split()[1].split("_")[0])
             new_day = min(last_day + 1, days)
             if (last_day == new_day):
                 continue
 
-        if not memeExistsForUser(userStat, new_day):
-            await send_message_safety(uid, f"Not enough memes for you :(", reply_markup=menu_kb)
+        if not memeExistsForUser(user, new_day):
+            await send_message_safety(user, f"Not enough memes for you :(", reply_markup=menu_kb)
             continue
 
         database.update(uid, winnerFlag=False)
 
-        await sendMemeToUser(userStat, new_day)
+        await sendMemeToUser(user, new_day)
 
-        await sendDailyQuestion(userStat, actual_nick)
+        await sendDailyQuestion(user, actual_nick)
     database.update()
 
 
-async def send_message_safety(chat_id, message, reply_markup, on_success = lambda: {}):
+async def send_message_safety(user, message, reply_markup, on_success = lambda: {}):
+    chat_id = user.uid
     try:
         await bot.send_message(chat_id, message, reply_markup=reply_markup)
         on_success()
-    except exceptions.BotBlocked as e:
-        noFapLogger.error(f"Target [ID:{chat_id}]: blocked by user")
+    except (BotBlocked, ChatNotFound) as err:
+        noFapLogger.error(f'"{err}" While sending to user {user.username}({user.uid})')
         database.update(chat_id, bannedFlag=True)
-    except exceptions.ChatNotFound as e:
-        noFapLogger.error(f"Target [ID:{chat_id}]: invalid user ID")
-        database.update(chat_id, bannedFlag=True)
-    except exceptions.RetryAfter as e:
-        noFapLogger.error(f"Target [ID:{chat_id}]: Flood limit is exceeded. Sleep {e.timeout} seconds.")
-        await asyncio.sleep(e.timeout)
-        return await send_message_safety(chat_id, message, reply_markup, on_success)
-    except exceptions.TelegramAPIError as e:
-        noFapLogger.error(f"Target [ID:{chat_id}]: failed")
+    except RetryAfter as err:
+        noFapLogger.error(f'"{err}" While sending to user {user.username}({user.uid})')
+        await asyncio.sleep(err.timeout)
+        return await send_message_safety(user, message, reply_markup, on_success)
+    except TelegramAPIError as err:
+        noFapLogger.error(f'"{err}" While sending to user {user.username}({user.uid})')
 
 
 async def send_photo_safety(chat_id, file_name):
