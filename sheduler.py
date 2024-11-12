c4d5fbb86426e14ab2b41d7a8a43437f33aa865f
TimurQQ <ilyasovtimur2001@gmail.com>
2024-11-13 01:23:37 +0300

    Repair triggers

--- sheduler.py
+++ sheduler.py
@@ -1,10 +1,17 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.interval import IntervalTrigger
 from database import database
 from src.handlers.daily_actions import checkRating
 from src.handlers.daily_actions import sendCheckMessageToWinners
 from logger import noFapLogger
 
 scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
-scheduler.add_job(noFapLogger.logDatabase, "cron", args=(database.data,), day_of_week='mon-sun', hour=21, minute = 00)
-scheduler.add_job(sendCheckMessageToWinners, "cron", day_of_week="mon-sun", hour=20, minute=00)
-scheduler.add_job(checkRating, "interval", seconds = 60)
+
+logging_trigger = CronTrigger(hour='21',minute='00')
+winners_trigger = CronTrigger(hour='20',minute='00')
+check_rating_trigger = IntervalTrigger(seconds = 60)
+
+scheduler.add_job(noFapLogger.logDatabase, trigger=logging_trigger)
+scheduler.add_job(sendCheckMessageToWinners, trigger=winners_trigger)
+scheduler.add_job(checkRating, trigger=check_rating_trigger)
